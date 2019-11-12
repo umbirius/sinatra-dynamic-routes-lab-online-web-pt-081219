371b8b23c0adb24d3b9351ee06c54b09c344e461
Umberto Palazzo <upalazzo93@gmail.com>
2019-11-12 20:19:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -29,8 +29,8 @@ class App < Sinatra::Base
   
   get '/:operation/:number1/:number2' do
     @operation = params[:operation]
-    @number1 = params[:number1].to_i
-    @number2 = params[:number2].to_i
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
     erb :operations
   end 
 
